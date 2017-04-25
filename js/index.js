709738262337bf9c2efa181ee9077ffb7d2f099e
William Lachance <wlachance@mozilla.com>
2017-04-25 17:01:06 -0400

    Use "real data", adding zooming view of usage khours vs. crash rate

--- js/index.js
+++ js/index.js
@@ -1,64 +1,24 @@
 "use strict";
 
-let fakeSpike = _.zip(_.range(1, 21),
-                      _.times(10, _.constant(100)).concat(_.times(10, _.constant(150)))).map(
-                          (tuple) => ({date: new Date('2017-04-' + tuple[0]),
-                                       value: tuple[1] + (Math.random()*5)}));
-
-let fakeSteady = _.zip(_.range(1, 21), _.times(20, _.constant(100))).map(
-    (tuple) => ({date: new Date('2017-04-' + tuple[0]), value: tuple[1] + (Math.random()*5)}));
-
-var crashCards = [
-    {
-        name: 'nightly-cpu',
-        danger: false,
-        data: fakeSteady
-    },
-    {
-        name: 'nightly-gpu',
-        danger: true,
-        data: fakeSpike
-    },
-    {
-        name: 'beta-cpu',
-        danger: false,
-        data: fakeSteady
-    },
-    {
-        name: 'beta-gpu',
-        danger: false,
-        data: fakeSteady
-    },
-    {
-        name: 'release-cpu',
-        danger: true,
-        data: fakeSpike
-    },
-    {
-        name: 'release-gpu',
-        danger: false,
-        data: fakeSteady
-    }
-];
+var crashCards = [];
 
 let render = function() {
     let filter = $('#filter-input').val();
 
     let row = 0, col = 0;
     $('#crash-cards').html(`<div class="row" id="crash-card-row-${row}"></div>`);
+    console.log(crashCards);
     crashCards.forEach(c => {
         // skip entries that don't match the filter
         if (filter.length && !c.name.includes(filter))
             return;
 
-        var cardClass = c.danger ? 'alert-danger' : 'alert-success';
         $(`#crash-card-row-${row}`).append(`
           <div class="col">
-            <div class="card ${cardClass}" style="width: 20rem;">
+            <div class="card" style="width: 20rem;">
               <div class="card-block card-content">
-                <h4 class="card-title">${c.name}</h4>
-                <div class="graph" id="${c.name}"></div>
-                <p>
+                <center><h4 class="card-title">${c.name}</h4></center>
+                <figure class="graph" id="${c.name}"></figure>
               </div>
             </div>
           </div>`);
@@ -68,9 +28,47 @@ let render = function() {
             height: 200,
             target: '#' + c.name,
             x_accessor: 'date',
-            y_accessor: 'value',
+            y_accessor: 'main_rate',
+            xax_format: d3.timeFormat('%Hh'),
             xax_count: 4
         });
+        $('#' + c.name).click(function() {
+            $('#crash-card-detail .modal-body').html(`
+             <div class="container">
+                <div class="row"><div class="col"><figure id="linked_${c.name}-enlarged"></figure></div></div>
+                <div class="row"><div class="col"><figure id="linked_${c.name}-khours-enlarged"></figure></div></div>
+             </div>`);
+
+            $('#crash-card-detail-title').html(`${c.name} details`);
+            MG.data_graphic({
+                title: "Main crash rate",
+                data: c.data,
+                width: 700,
+                height: 300,
+                target: `#linked_${c.name}-enlarged`,
+                x_accessor: 'date',
+                y_accessor: 'main_rate',
+                xax_format: d3.timeFormat('%Hh'),
+                show_secondary_x_label: false,
+                linked: true,
+                linked_format: "%Y-%m-%d-%H-%M-%S"
+            });
+            MG.data_graphic({
+                title: "Usage khours",
+                data: c.data,
+                width: 700,
+                height: 200,
+                target: `#linked_${c.name}-khours-enlarged`,
+                x_accessor: 'date',
+                y_accessor: 'usage_khours',
+                xax_format: d3.timeFormat('%Hh'),
+                show_secondary_x_label: false,
+                linked: true,
+                linked_format: "%Y-%m-%d-%H-%M-%S"
+            });
+            $('#crash-card-detail').modal({});
+        });
+
         col++;
         if (col > 2) {
             row++;
@@ -84,7 +82,40 @@ let render = function() {
     }
 }
 
-render();
+fetch('https://sql.telemetry.mozilla.org/api/queries/4351/results.json?api_key=WIbd6HdaP3I9vzgTp28vMHLZXLY2VaVl7bDNBchs').then(
+    function(response) {
+	// Convert to JSON
+	return response.json();
+    }).then(function(data) {
+        console.log(data);
+        let crashCardsMap = {};
+        data.query_result.data.rows.forEach(row => {
+            let osname = row.os_name;
+            let channel = row.channel;
+            if (!crashCardsMap[osname]) {
+                crashCardsMap[osname] = {};
+            }
+            if (!crashCardsMap[osname][channel]) {
+                crashCardsMap[osname][channel] = {
+                    name: `${osname}-${channel}`,
+                    data: []
+                };
+            }
+
+            crashCardsMap[osname][channel].data.push({
+                main_rate: row.main_rate,
+                usage_khours: row.usage_khours,
+                date: new Date(row.date)
+            });
+        });
+
+        crashCards = _.flattenDeep(_.values(crashCardsMap).map(channelMap => _.values(channelMap)));
+        crashCards.forEach(c => {
+            c.data.sort(d => { return d.date; });
+        });
+        render();
+    });
+
 
 $('#filter-input').on('input', function() {
     render();
