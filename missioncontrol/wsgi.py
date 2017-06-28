597d7f7f3f6579af18eadd9254955aba12eb0fb8
Wesley Dawson <whd@mozilla.com>
2017-06-28 15:32:19 -0700

    Add missing import

--- missioncontrol/wsgi.py
+++ missioncontrol/wsgi.py
@@ -12,6 +12,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'missioncontrol.settings')  # NO
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
