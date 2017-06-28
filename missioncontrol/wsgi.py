262e91c723cd3960901486eb4d22ef953bcb6276
Wesley Dawson <whd@mozilla.com>
2017-06-28 12:19:57 -0700

    Don't explicitly set newrelic configuration

--- missioncontrol/wsgi.py
+++ missioncontrol/wsgi.py
@@ -12,10 +12,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'missioncontrol.settings')  # NO
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
-import newrelic.agent
-from decouple import config
-from whitenoise.django import DjangoWhiteNoise
-
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
@@ -23,10 +19,3 @@ application = DjangoWhiteNoise(application)
 # Add media files
 if settings.MEDIA_ROOT and settings.MEDIA_URL:
     application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
-
-# Add NewRelic
-newrelic_ini = config('NEW_RELIC_CONFIG_FILE', default='newrelic.ini')
-newrelic_license_key = config('NEW_RELIC_LICENSE_KEY', default=None)
-if newrelic_ini and newrelic_license_key:
-    newrelic.agent.initialize(newrelic_ini)
-    application = newrelic.agent.wsgi_application()(application)
