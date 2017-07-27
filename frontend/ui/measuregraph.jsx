5486099a84d3d0a1cbbaf5641d5c998c60de7678
William Lachance <wlachance@mozilla.com>
2017-07-27 14:37:46 -0400

    Remove unnecessary state handling in measuregraph component

--- frontend/ui/measuregraph.jsx
+++ frontend/ui/measuregraph.jsx
@@ -4,30 +4,20 @@ import { timeFormat } from 'd3-time-format';
 import MetricsGraphics from 'react-metrics-graphics';
 
 export default class MeasureGraph extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: this.props.title,
-      y: this.props.y || 'value',
-      width: this.props.width,
-      height: this.props.height
-    };
-  }
-
   render() {
     const numSeries = this.props.seriesList.length;
     return (
       <MetricsGraphics
-        title={this.state.title}
+        title={this.props.title}
         chart_type={!numSeries ? 'missing-data' : undefined}
         legend={numSeries > 1 ? this.props.seriesList.map(s => s.name) : undefined}
         data={numSeries ? this.props.seriesList.map(s => s.data) : undefined}
-        width={this.state.width}
-        height={this.state.height}
+        width={this.props.width}
+        height={this.props.height}
         interpolate={curveLinear}
         missing_text="No data for this measure"
         x_accessor="date"
-        y_accessor={this.state.y}
+        y_accessor={this.props.y || 'value'}
         xax_format={this.props.xax_format ? timeFormat(this.props.xax_format) : undefined}
         xax_count={this.props.xax_count}
         linked={this.props.linked}
