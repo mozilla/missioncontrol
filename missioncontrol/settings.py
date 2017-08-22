3af31ccc469278508d4f4fee1b0d9f16a1ce50fa
William Lachance <wrlach@gmail.com>
2017-08-22 13:11:42 -0400

    Explicitly configure/set cache timeout of accessing the version endpoint (#114)
    We want it to be fairly low, as we want to display changes quickly.

--- missioncontrol/settings.py
+++ missioncontrol/settings.py
@@ -309,5 +309,6 @@ LOGGING = {
 }
 
 FIREFOX_VERSION_URL = 'https://product-details.mozilla.org/1.0/firefox_versions.json'
+FIREFOX_VERSION_CACHE_TIMEOUT = 300
 
 DATA_EXPIRY_INTERVAL = timedelta(days=30)
