a481bcfcda99bb58c8f0cce051945629ea220667
William Lachance <wlachance@mozilla.com>
2018-01-23 13:40:00 -0500

    Fix update_experiment_list management command

--- missioncontrol/etl/management/commands/update_experiment_list.py
+++ missioncontrol/etl/management/commands/update_experiment_list.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from missioncontrol.etl.tasks import update_active_experiments
+from missioncontrol.etl.tasks import update_experiment_list
 
 
 class Command(BaseCommand):
     """Management command to update data for a specific experiment"""
 
     def handle(self, *args, **options):
-        update_active_experiments()
+        update_experiment_list()
