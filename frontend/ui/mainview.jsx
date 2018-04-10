1054b003b17eeea627f655a748fbe89549748946
William Lachance <wlachance@mozilla.com>
2018-04-10 14:05:34 -0400

    Calculate summary rates (fixes #203, #250)
    Both a summary rate for each platform/channel combo and an overall rate
    (geometric mean of all of them)

--- frontend/ui/mainview.jsx
+++ frontend/ui/mainview.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import numeral from 'numeral';
 import React from 'react';
 import { Helmet } from 'react-helmet';
@@ -14,11 +15,56 @@ import { connect } from 'react-redux';
 import Loading from './loading';
 import { CHANNEL_ICON_MAPPING, KEY_MEASURES } from '../schema';
 
-// eventually this will load a summary for each channel/os combination, so
-// leaving this in for now even though were not using it
-const mapStateToProps = state => ({
-  channelPlatformSummary: state.channelPlatformSummary,
-});
+const mapStateToProps = state => {
+  if (state.channelPlatformSummary && state.channelPlatformSummary.summaries) {
+    return {
+      channelPlatformSummary: {
+        ...state.channelPlatformSummary,
+        summaries: state.channelPlatformSummary.summaries.map(summary => {
+          // only pretend we have a summary rate if we've got all the key
+          // measures for a particular channel/platform combo
+          const summaryRate = _.every(
+            KEY_MEASURES.map(keyMeasure =>
+              summary.measures.map(m => m.name).includes(keyMeasure)
+            )
+          )
+            ? _.sum(
+                summary.measures
+                  .filter(m => KEY_MEASURES.includes(m.name))
+                  .map(m => m.versions[1].adjustedRate)
+              ).toFixed(2)
+            : undefined;
+
+          return {
+            ...summary,
+            summaryRate,
+          };
+        }),
+      },
+    };
+  }
+
+  return {
+    channelPlatformSummary: state.channelPlatformSummary,
+  };
+};
+
+const getSummaryRate = summaries => {
+  if (!_.every(summaries.map(summary => summary.summaryRate))) {
+    return 'N/A';
+  }
+
+  const summaryRates = summaries.map(summary => summary.summaryRate);
+
+  return (
+    <abbr title={`geometric mean of rates: ${summaryRates.join(' * ')}`}>
+      {numeral(summaryRates.reduce((total, curr) => total * curr)).format(
+        '0.00'
+      )}
+    </abbr>
+  );
+};
+
 const getChangeIndicator = versions => {
   if (versions.length > 2) {
     const pctChange =
@@ -110,61 +156,86 @@ class MainViewComponent extends React.Component {
         <div className="container center">
           {this.state.isLoading && <Loading />}
           {!this.state.isLoading && (
-            <CardColumns>
-              {this.props.channelPlatformSummary &&
-                this.props.channelPlatformSummary.summaries.map(
-                  summary =>
-                    summary.channel === this.state.channel && (
-                      <Card
-                        key={`${summary.platform}-${summary.channel}`}
-                        onClick={() =>
-                          this.cardClicked(summary.channel, summary.platform)
-                        }
-                        className="missioncontrol-card">
-                        <CardHeader className={`alert-${summary.status}`}>
-                          {summary.platform}
-                        </CardHeader>
-                        <CardBody>
-                          <table className="table table-sm summary-table">
-                            <tbody>
-                              {summary.measures
-                                .filter(
-                                  measure =>
-                                    KEY_MEASURES.includes(measure.name) &&
-                                    measure.versions.length
-                                )
-                                .map(measure => (
-                                  <tr
-                                    key={measure.name}
-                                    title={
-                                      measure.versions.length > 1
-                                        ? `Average ${
-                                            measure.versions[1].adjustedRate
-                                          } events per 1000 hours`
-                                        : ''
-                                    }>
-                                    <td>{measure.name}</td>
-                                    <td
-                                      id={`${summary.platform}-${
-                                        summary.channel
-                                      }-${measure.name}`}
-                                      align="right">
-                                      {measure.versions.length > 1 &&
-                                        measure.versions[1].adjustedRate}
-                                    </td>
-                                    <td align="right">
-                                      {measure.versions &&
-                                        getChangeIndicator(measure.versions)}
-                                    </td>
-                                  </tr>
-                                ))}
-                            </tbody>
-                          </table>
-                        </CardBody>
-                      </Card>
-                    )
-                )}
-            </CardColumns>
+            <div>
+              <div className="container">
+                <div className="row">
+                  <div className="col align-self-center channel-summary">
+                    <div className="channel-summary-title">Summary Score</div>
+                    <div className="channel-summary-score">
+                      <abbr title="geometric mean of child rates">
+                        {getSummaryRate(
+                          this.props.channelPlatformSummary.summaries.filter(
+                            s => s.channel === this.state.channel
+                          )
+                        )}
+                      </abbr>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <CardColumns>
+                {this.props.channelPlatformSummary &&
+                  this.props.channelPlatformSummary.summaries.map(
+                    summary =>
+                      summary.channel === this.state.channel && (
+                        <Card
+                          key={`${summary.platform}-${summary.channel}`}
+                          onClick={() =>
+                            this.cardClicked(summary.channel, summary.platform)
+                          }
+                          className="missioncontrol-card">
+                          <CardHeader className={`alert-${summary.status}`}>
+                            {summary.platform}
+                          </CardHeader>
+                          <CardBody>
+                            <div className="summary-rate">
+                              <abbr title="main_crashes + content_crashes">
+                                {summary.summaryRate
+                                  ? summary.summaryRate
+                                  : 'N/A'}
+                              </abbr>
+                            </div>
+                            <table className="table table-sm summary-table">
+                              <tbody>
+                                {summary.measures
+                                  .filter(
+                                    measure =>
+                                      KEY_MEASURES.includes(measure.name) &&
+                                      measure.versions.length
+                                  )
+                                  .map(measure => (
+                                    <tr
+                                      key={measure.name}
+                                      title={
+                                        measure.versions.length > 1
+                                          ? `Average ${
+                                              measure.versions[1].adjustedRate
+                                            } events per 1000 hours`
+                                          : ''
+                                      }>
+                                      <td>{measure.name}</td>
+                                      <td
+                                        id={`${summary.platform}-${
+                                          summary.channel
+                                        }-${measure.name}`}
+                                        align="right">
+                                        {measure.versions.length > 1 &&
+                                          measure.versions[1].adjustedRate}
+                                      </td>
+                                      <td align="right">
+                                        {measure.versions &&
+                                          getChangeIndicator(measure.versions)}
+                                      </td>
+                                    </tr>
+                                  ))}
+                              </tbody>
+                            </table>
+                          </CardBody>
+                        </Card>
+                      )
+                  )}
+              </CardColumns>
+            </div>
           )}
         </div>
       </div>
