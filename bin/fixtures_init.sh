3788649451a08a7e5c06b80e738245a75796cae2
William Lachance <wlachance@mozilla.com>
2018-05-14 15:28:45 -0400

    Update to use error_aggregates_v2 by default

--- bin/fixtures_init.sh
+++ bin/fixtures_init.sh
@@ -4,4 +4,4 @@ docker-compose exec metastore hadoop fs -mkdir -p /user/hive/warehouse
 docker cp sample.snappy.parquet "$(docker-compose ps -q metastore)":/tmp/sample.snappy.parquet
 docker cp fixtures_init.sql "$(docker-compose ps -q presto)":/tmp/fixtures_init.sql
 docker-compose exec presto presto-cli -f /tmp/fixtures_init.sql
-docker-compose exec metastore hadoop fs -copyFromLocal /tmp/sample.snappy.parquet /user/hive/warehouse/error_aggregates_v1/sample.snappy.parquet
+docker-compose exec metastore hadoop fs -copyFromLocal /tmp/sample.snappy.parquet /user/hive/warehouse/error_aggregates_v2/sample.snappy.parquet
