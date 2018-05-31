c829a89bf4baac8f3aa08931be824854c8ccd5c9
William Lachance <wlachance@mozilla.com>
2018-05-31 16:14:03 -0400

    Start gathering data for fennec beta (fixes #276)

--- missioncontrol/base/management/commands/load_initial_data.py
+++ missioncontrol/base/management/commands/load_initial_data.py
@@ -76,8 +76,6 @@ class Command(BaseCommand):
             application=Application.objects.get(name='fennec'),
             platform=Platform.objects.get(name='android'),
             defaults={'enabled': True})
-        # there is no android/fennec on esr, and we are temporarily
-        # disabling gathering crash data on beta:
-        # https://github.com/mozilla/missioncontrol/issues/276
-        measure.channels = Channel.objects.exclude(name__in=['beta', 'esr'])
+        # there is no android/fennec on esr
+        measure.channels = Channel.objects.exclude(name__in=['esr'])
         measure.save()
