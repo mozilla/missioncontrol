b42c12d0a3ce964784c6356859bb961db17302e1
William Lachance <wlachance@mozilla.com>
2018-06-06 16:28:26 -0400

    Make platform/channel cards links (fixes: #207)
    This enables users to quickly open up a bunch of related data

--- frontend/ui/platformcard.jsx
+++ frontend/ui/platformcard.jsx
@@ -51,64 +51,71 @@ class PlatformCard extends React.Component {
     const { summary } = this.props;
 
     return (
-      <Card onClick={() => this.cardClicked()} className="missioncontrol-card">
-        <CardHeader className={`alert-${summary.status}`}>
-          <center>
-            {`${_.capitalize(summary.application)} ${_.capitalize(
-              summary.platform
-            )} `}
-            {summary.latestVersionSeen && (
-              <small className="text-muted">
-                ({summary.latestVersionSeen})
-              </small>
-            )}
-          </center>
-        </CardHeader>
-        <CardBody>
-          <div className="summary-rate">
-            <abbr
-              title={_.intersection(
-                summary.expectedMeasures,
-                KEY_MEASURES
-              ).join(' + ')}>
-              {summary.summaryRate ? summary.summaryRate : 'N/A'}
-            </abbr>
-          </div>
-          <table className="table table-sm summary-table">
-            <tbody>
-              {summary.measures
-                .filter(
-                  measure =>
-                    KEY_MEASURES.includes(measure.name) &&
-                    measure.versions.length &&
-                    measure.versions[0].version === summary.latestVersionSeen
-                )
-                .map(measure => (
-                  <tr
-                    key={`${summary.application}-${summary.platform}-${
-                      measure.name
-                    }`}
-                    title={
-                      measure.versions.length > 1
-                        ? `Average ${
-                            measure.versions[1].adjustedRate
-                          } events per 1000 hours`
-                        : ''
-                    }>
-                    <td>{measure.name}</td>
-                    <td align="right">
-                      {measure.versions.length > 1 &&
-                        measure.versions[1].adjustedRate}
-                    </td>
-                    <td align="right">
-                      {measure.versions && getChangeIndicator(measure.versions)}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </CardBody>
-      </Card>
+      <a
+        href={`#/${summary.channel}/${summary.platform}`}
+        className="missioncontrol-card-link">
+        <Card
+          onClick={() => this.cardClicked()}
+          className="missioncontrol-card">
+          <CardHeader className={`alert-${summary.status}`}>
+            <center>
+              {`${_.capitalize(summary.application)} ${_.capitalize(
+                summary.platform
+              )} `}
+              {summary.latestVersionSeen && (
+                <small className="text-muted">
+                  ({summary.latestVersionSeen})
+                </small>
+              )}
+            </center>
+          </CardHeader>
+          <CardBody>
+            <div className="summary-rate">
+              <abbr
+                title={_.intersection(
+                  summary.expectedMeasures,
+                  KEY_MEASURES
+                ).join(' + ')}>
+                {summary.summaryRate ? summary.summaryRate : 'N/A'}
+              </abbr>
+            </div>
+            <table className="table table-sm summary-table">
+              <tbody>
+                {summary.measures
+                  .filter(
+                    measure =>
+                      KEY_MEASURES.includes(measure.name) &&
+                      measure.versions.length &&
+                      measure.versions[0].version === summary.latestVersionSeen
+                  )
+                  .map(measure => (
+                    <tr
+                      key={`${summary.application}-${summary.platform}-${
+                        measure.name
+                      }`}
+                      title={
+                        measure.versions.length > 1
+                          ? `Average ${
+                              measure.versions[1].adjustedRate
+                            } events per 1000 hours`
+                          : ''
+                      }>
+                      <td>{measure.name}</td>
+                      <td align="right">
+                        {measure.versions.length > 1 &&
+                          measure.versions[1].adjustedRate}
+                      </td>
+                      <td align="right">
+                        {measure.versions &&
+                          getChangeIndicator(measure.versions)}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </CardBody>
+        </Card>
+      </a>
     );
   }
 }
