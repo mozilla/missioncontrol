e8a2cec397e8fddf3f00628aa9d50267b2075a1e
William Lachance <wlachance@mozilla.com>
2018-07-17 17:10:41 -0400

    Show rates for last few point/beta releases in summary view (fixes #249)

--- missioncontrol/etl/measuresummary.py
+++ missioncontrol/etl/measuresummary.py
@@ -77,74 +77,85 @@ def get_measure_summary(application_name, platform_name, channel_name, measure_n
             grouped_versions[major_version] = []
         grouped_versions[major_version].append(
             (version, min_timestamp, max_timestamp))
-    versions = []
 
+    major_versions = []
     for major_version in sorted(grouped_versions.keys()):
         subversions = sorted(grouped_versions[major_version], key=lambda d: parse_version(d[0]))
-        versions.append((str(major_version), subversions[0][1], subversions[-1][2]))
+        major_versions.append((str(major_version), subversions[0][1], subversions[-1][2]))
 
-    latest_version_interval = versions[-1][2] - versions[-1][1]
+    latest_major_version_interval = major_versions[-1][2] - major_versions[-1][1]
 
-    # we tack the latest version on at the end just to give people an idea of
-    # what's happening with the latest point release / beta
-    versions.append(raw_version_data[-1])  # always include the latest full version in our list
+    def _get_version_summaries(version_data, reference_duration):
+        version_summaries = []
+        # this somewhat confusing bit of code tries to determine the end of each major
+        # version programatically by looking at when the next one started (we take the
+        # latest major version and the latest version at face value -- i.e. what we
+        # see in the data)
+        for (version, next_version) in zip(version_data, version_data[1:] + [None, None]):
+            version, next_version
+            version_start = version[1]
 
-    # this somewhat confusing bit of code tries to determine the end of each major
-    # version programatically by looking at when the next one started (we take the
-    # latest major version and the latest version at face value -- i.e. what we
-    # see in the data)
-    version_summaries = []
-    for (version, next_version) in zip(versions, versions[1:-1] + [None, None]):
-        version_start = version[1]
+            if next_version is not None:
+                version_end = next_version[1]
+            else:
+                version_end = version[2]
 
-        if next_version is not None:
-            version_end = next_version[1]
-        else:
-            version_end = version[2]
+            field_duration = version_end - version_start
+            adjusted_duration = reference_duration or field_duration
 
-        field_duration = version_end - version_start
+            # if we can, ignore the first 24 hours as it tends to be very noisy
+            # (we only want to do this if the latest version has been out for more than 24 hours)
+            if ((latest_major_version_interval and
+                latest_major_version_interval > datetime.timedelta(days=1)) and
+                field_duration > datetime.timedelta(days=2)):
+                version_start = version[1] + datetime.timedelta(days=1)
 
-        # if we can, ignore the first 24 hours as it tends to be very noisy
-        # (we only want to do this if the latest version has been out for more than 24 hours)
-        if (latest_version_interval > datetime.timedelta(days=1) and
-            field_duration > datetime.timedelta(days=2)):
-            version_start = version[1] + datetime.timedelta(days=1)
+            if version_start >= version_end:
+                # this version was either super-short lived or we have truncated
+                # data set for it, in either case we shouldn't pretend that we can
+                # provide a valid summary of it
+                continue
+            else:
+                version_summary = {
+                    'version': version[0],
+                    'fieldDuration': int(field_duration.total_seconds())
+                }
+                for (rate_id, count_id, interval) in (
+                        ('rate', 'count', field_duration),
+                        ('adjustedRate', 'adjustedCount', adjusted_duration)
+                ):
+                    values = _get_data_interval_for_version(builds, measure,
+                                                            version[0],
+                                                            version_start,
+                                                            version_start + interval)
+                    if not values:
+                        # in rare cases (mostly during backfilling) we might not
+                        # have any actual data for the version in question in the
+                        # interval we want
+                        continue
+                    raw_count = int(sum([v[1] for v in values]))
+                    version_summary[count_id] = raw_count
 
-        if version_start >= version_end:
-            # this version was either super-short lived or we have truncated
-            # data set for it, in either case we shouldn't pretend that we can
-            # provide a valid summary of it
-            continue
-        else:
-            version_data = {
-                'version': version[0],
-                'fieldDuration': int(field_duration.total_seconds())
-            }
-            for (rate_id, count_id, interval) in (
-                    ('rate', 'count', version_end - version_start),
-                    ('adjustedRate', 'adjustedCount', latest_version_interval)
-            ):
-                values = _get_data_interval_for_version(builds, measure,
-                                                        version[0],
-                                                        version_start,
-                                                        version_start + interval)
-                if not values:
-                    # in rare cases (mostly during backfilling) we might not
-                    # have any actual data for the version in question in the
-                    # interval we want
-                    continue
-                raw_count = int(sum([v[1] for v in values]))
-                version_data[count_id] = raw_count
+                    # to prevent outliers from impacting our rate calculation, we'll use
+                    # the 99th percentile of captured values
+                    end = math.ceil(len(values) * 0.99)
+                    rate_values = values[:end]
+                    version_summary[rate_id] = round(
+                        sum([v[1] for v in rate_values]) /
+                        sum([v[2]/1000.0 for v in rate_values]), 2)
 
-                # to prevent outliers from impacting our rate calculation, we'll use
-                # the 99th percentile of captured values
-                end = math.ceil(len(values) * 0.99)
-                rate_values = values[:end]
-                version_data[rate_id] = round(
-                    sum([v[1] for v in rate_values]) /
-                    sum([v[2]/1000.0 for v in rate_values]), 2)
+                version_summaries.append(version_summary)
 
-            version_summaries.append(version_data)
+        return version_summaries
+
+    version_summaries = _get_version_summaries(major_versions,
+                                               latest_major_version_interval)
+
+    # we tack the last few versions on at the end just to give people an idea of
+    # what's happening with the last few point releases / betas (except on
+    # nightly where we basically have a continuous flow of new releases)
+    if channel_name is not 'nightly':
+        version_summaries.extend(_get_version_summaries(raw_version_data[-3:], None))
 
     if not version_summaries:
         return None
